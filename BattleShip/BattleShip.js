0198942e8d15a7a874d04e046c21b476975a034a
Michael Lu <yeskky@gmail.com>
2018-02-07 14:28:40 +0800

    Create View object and test it.

--- BattleShip/BattleShip.js
+++ BattleShip/BattleShip.js
@@ -1,18 +1,54 @@
 $(function () {
     createSeaTable();
+    testView();
+
 });
+
+function testView() {
+    view.displayMiss("00");
+    view.displayHit("34");
+    view.displayMiss("55");
+    view.displayHit("12");
+    view.displayMiss("25");
+    view.displayHit("26");
+}
 
 /**
  * 创建格子
  */
-function createSeaTable()
-{
+function createSeaTable() {
     for (var i = 0; i < 7; i++) {
         var trHTML = "<tr>";
         for (var j = 0; j < 7; j++) {
             trHTML += "<td id='" + i + j + "' class=''></td>";
         }
-        trHTML+="</tr>";
+        trHTML += "</tr>";
         $("#seaTable").append(trHTML);
+    }
+}
+
+/**
+ * View 对象，负责界面更新
+ */
+var view = {
+    displayMessage: function (msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    /**
+     * location 是单元格的 id
+     */
+    displayHit: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+
+    /**
+     * location 是单元格的 id
+     */
+    displayMiss: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
     }
 }
